52cd8d38885efd71e8e857d4889fb79762e9ec86
danielr <daniel.rodak@untitledkingdom.com>
2018-11-09 09:57:25 +0100

    36 done

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle34.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle34.kt
@@ -9,7 +9,7 @@ object Riddle34 {
    *
    * Use case: You want to shift work to a particular [Scheduler].
    */
-  fun solve(source: Completable, scheduler: Scheduler): Completable {
-    TODO()
-  }
+  fun solve(source: Completable, scheduler: Scheduler): Completable =
+          source.observeOn(scheduler)
+          .subscribeOn(scheduler)
 }
